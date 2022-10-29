058cd83712502861d4bb4061710fa3b122fc6c06
Alex Turaev <altex_76@mail.ru>
2022-10-29 15:30:47 +0300

    first commit

---
+++ src/main/java/hw2_15/com/example/hw2_15_demo/Hw215DemoApplication.java
@@ -0,0 +1,13 @@
+package hw2_15.com.example.hw2_15_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw215DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw215DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hw2_15/com/example/hw2_15_demo/Hw215DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package hw2_15.com.example.hw2_15_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw215DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
